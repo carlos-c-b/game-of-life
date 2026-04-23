7887d06884982322456f95c89ae1c44c559d1e41
carlos-c-b <carloscbc2004@gmail.com>
2026-04-23 11:42:49 +0200

    Grid created and clicking works

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# game-of-lide
+# game-test
 
 A [libGDX](https://libgdx.com/) project generated with [gdx-liftoff](https://github.com/libgdx/gdx-liftoff).
 
--- core/src/main/java/com/carlos/Main.java
+++ core/src/main/java/com/carlos/Main.java
@@ -1,32 +1,87 @@
 package com.carlos;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+    Texture cellTexture;
+    Texture deadcellTexture;
+    boolean isAlive[][];
+    int cellSize = 1;
+    int worldWidth = 8;
+    int worldHeight = 8;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(worldWidth, worldHeight);
+        cellTexture = new Texture("cell.png");
+        deadcellTexture = new Texture("deadcell.png");
+        isAlive = new boolean[worldHeight][worldWidth];
+
+
+        for(int i = 0; i < worldWidth; i++) {
+            for(int j = 0; j < worldHeight; j++) {
+                isAlive[i][j] = true;
+            }
+        }
+        
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        input();
+        logic();
+        draw();
+    }
+
+    public void input() {
+        if (Gdx.input.justTouched()) {  // justTouched fires once per click
+            // Convert screen coords to world coords
+            Vector2 worldCoords = viewport.unproject(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
+            
+            int row = (int) worldCoords.x / cellSize;
+            int col = (int) worldCoords.y / cellSize;
+
+            if(row <= worldHeight && col <= worldWidth)
+                isAlive[row][col] = !isAlive[row][col];
+
+        }
+    }
+
+    public void logic() {
+
+    }
+
+    public void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        for(int i = 0; i < worldWidth; i++) {
+            for(int j = 0; j < worldHeight; j++) {
+                Texture tex = isAlive[i][j] ? cellTexture : deadcellTexture;
+                spriteBatch.draw(tex, i, j, cellSize, cellSize);
+            }
+        }
+
+        spriteBatch.end();
     }
 
     @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
     }
+
 }
