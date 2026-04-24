1b06cd1ceb3ea119f10d3954073e45dd5e19c9ca
carlos-c-b <carloscbc2004@gmail.com>
2026-04-24 11:35:18 +0200

    Check rules logic

--- core/src/main/java/com/carlos/Main.java
+++ core/src/main/java/com/carlos/Main.java
@@ -2,6 +2,7 @@ package com.carlos;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -16,10 +17,16 @@ public class Main extends ApplicationAdapter {
     FitViewport viewport;
     Texture cellTexture;
     Texture deadcellTexture;
+    Texture buttonTexture;
     boolean isAlive[][];
+    boolean next[][];
     int cellSize = 1;
     int worldWidth = 8;
     int worldHeight = 8;
+    boolean preparing = true;
+    float step = 0.5f;
+    float timer = 0f;
+    int aliveCounter;
 
     @Override
     public void create() {
@@ -28,40 +35,65 @@ public class Main extends ApplicationAdapter {
         cellTexture = new Texture("cell.png");
         deadcellTexture = new Texture("deadcell.png");
         isAlive = new boolean[worldHeight][worldWidth];
+        next = new boolean[worldHeight][worldWidth];
 
 
         for(int i = 0; i < worldWidth; i++) {
             for(int j = 0; j < worldHeight; j++) {
-                isAlive[i][j] = true;
+                isAlive[i][j] = false;
             }
         }
+        aliveCounter = 0;
         
     }
 
     @Override
     public void render() {
         input();
-        logic();
+        if(!preparing)
+            logic();
         draw();
     }
 
     public void input() {
-        if (Gdx.input.justTouched()) {  // justTouched fires once per click
+        if (Gdx.input.justTouched() && preparing) {  // justTouched fires once per click
             // Convert screen coords to world coords
             Vector2 worldCoords = viewport.unproject(new Vector2(Gdx.input.getX(), Gdx.input.getY()));
             
             int row = (int) worldCoords.x / cellSize;
             int col = (int) worldCoords.y / cellSize;
 
-            if(row <= worldHeight && col <= worldWidth)
+            if(row <= worldHeight && col <= worldWidth) {
                 isAlive[row][col] = !isAlive[row][col];
+                if(isAlive[row][col]) aliveCounter++;
+                else aliveCounter--;
+            }
 
+        } else if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+            if(preparing) {
+                preparing = false;
+            } else {
+                restart();
+            }
         }
     }
 
-    public void logic() {
-
+    public void restart() {
+        for(int i = 0; i < worldHeight; i++)
+            for(int j = 0; j < worldWidth; j++) isAlive[i][j] = false;
+        aliveCounter = 0;
+        preparing = true;
     }
+
+    public void logic() {
+        timer += Gdx.graphics.getDeltaTime();
+        if(timer >= step) {
+            timer -= step;
+            nextGeneration();
+            if(aliveCounter == 0) preparing = true;
+        }
+    }
+
 
     public void draw() {
         ScreenUtils.clear(Color.BLACK);
@@ -69,14 +101,49 @@ public class Main extends ApplicationAdapter {
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
 
-        for(int i = 0; i < worldWidth; i++) {
-            for(int j = 0; j < worldHeight; j++) {
+        for(int i = 0; i < worldHeight; i++) {
+            for(int j = 0; j < worldWidth; j++) {
                 Texture tex = isAlive[i][j] ? cellTexture : deadcellTexture;
                 spriteBatch.draw(tex, i, j, cellSize, cellSize);
             }
         }
+    
 
         spriteBatch.end();
+    }
+
+    public int countNeighbours(int i, int j) {
+        int count = 0;
+        if(i > 0 && isAlive[i-1][j]) count++;
+        if(i < worldHeight-1 && isAlive[i+1][j]) count++;
+        if(j > 0 && isAlive[i][j-1]) count++;
+        if(j < worldWidth-1 && isAlive[i][j+1]) count++;
+        return count;
+    }
+
+    /*
+    *   1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    *   2. Any live cell with two or three live neighbours lives on to the next generation.
+    *   3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+    *   4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    */
+    public void nextGeneration() {
+        for(int i = 0; i < worldHeight; i++) {
+            for(int j = 0; j < worldWidth; j++) {
+                int neighbours = countNeighbours(i,j);
+                if(isAlive[i][j])
+                    next[i][j] = neighbours == 2 || neighbours == 3;
+                else
+                    next[i][j] = neighbours == 3;
+                
+                // Update aliveCounter
+                if(isAlive[i][j] && !next[i][j]) aliveCounter--;
+                else if(!isAlive[i][j] && next[i][j]) aliveCounter++;
+            }
+        }
+        boolean[][] tmp = isAlive;
+        isAlive = next;
+        next = tmp;     // We bypass the garbage collector by storing the previous state array in next
     }
 
     @Override
